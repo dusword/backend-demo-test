92ec935a371dd369aa60b6ecb2101f5f0f68a8ee
李祥捷 <773983534@qq.com>
2021-11-20 22:21:53 +0800

    改变swagger配置位置 --lxj

--- src/main/java/com/example/demo/config/Swagger2.java
+++ src/main/java/com/example/demo/config/Swagger2.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
